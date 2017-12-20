f7d7a1adc70542a5d121b0fb441339f96fbf7649
程挥竣 <chenghuijun@shhxzq.com>
2017-12-20 11:32:53 +0800

    加入offset属性

--- src/index.js
+++ src/index.js
@@ -76,6 +76,7 @@ class stockChartKline extends Component {
       barChartHeight,
       chartMargin,
       showGrid,
+      offset,
       backgroundColor
     } = this.props;
     const xScaleProvider = scale.discontinuousTimeScaleProvider.inputDateAccessor(d => d.date);
@@ -98,7 +99,7 @@ class stockChartKline extends Component {
           <Chart id={1} yExtents={[d => [d.high, d.low, d.MA5, d.MA10, d.MA30]]} height={lineChartHeight} origin={(w, h) => [0, 0]}>
             <axes.XAxis axisAt="bottom" orient="bottom" ticks={1} zoomEnabled={false} showTicks={false} showDomain={false} />
             <axes.YAxis axisAt="right" orient="right" ticks={3} zoomEnabled={false} showTickLabel={false} {...yGrid} showDomain={false} />
-            <series.CandlestickSeries />
+            <series.CandlestickSeries offset={offset} />
             <series.LineSeries yAccessor={d => d.MA5} stroke="white" />
 
             <series.LineSeries yAccessor={d => d.MA10} stroke="yellow" />
@@ -121,6 +122,7 @@ class stockChartKline extends Component {
                         stroke: 'magenta'
                     }
                     ])} fontSize={15}
+                offset={offset}
             />
           </Chart>
           <Chart id={2} yExtents={[d => d.volume]} height={barChartHeight} origin={(w, h) => [0, h - 40]}>
@@ -131,6 +133,7 @@ class stockChartKline extends Component {
                 }} fill={d => {
                   return d.volume ? d.volumeColor : '#393c43';
                 }}
+                offset={offset}
             />
           </Chart>
         </ChartCanvas>
@@ -153,6 +156,7 @@ stockChartKline.propTypes = {
     bottom: PropTypes.number
   }),
   showGrid: PropTypes.bool,
+  offset: PropTypes.number,
   backgroundColor: PropTypes.string
 };
 
@@ -160,6 +164,7 @@ stockChartKline.defaultProps = {
   type: 'hybrid',
   lineChartHeight: 168,
   barChartHeight: 40,
+  offset: 3,
   chartMargin: {
     left: 5, right: 5, top: 10, bottom: 0
   },
